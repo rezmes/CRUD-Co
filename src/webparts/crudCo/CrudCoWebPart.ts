233530dfacbe6b3fbaf57d24d9e1e921f48d1acb
rezmes <rezmes@gmail.com>
2024-07-14 10:24:22 +0330

    Raw spfx@1.4.1

---
+++ src/webparts/crudCo/CrudCoWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'CrudCoWebPartStrings';
+import CrudCo from './components/CrudCo';
+import { ICrudCoProps } from './components/ICrudCoProps';
+
+export interface ICrudCoWebPartProps {
+  description: string;
+}
+
+export default class CrudCoWebPart extends BaseClientSideWebPart<ICrudCoWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ICrudCoProps > = React.createElement(
+      CrudCo,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/crudCo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICrudCoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CrudCoWebPartStrings' {
+  const strings: ICrudCoWebPartStrings;
+  export = strings;
+}
